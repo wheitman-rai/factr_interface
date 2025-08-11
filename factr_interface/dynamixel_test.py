02e3af8615de044c52f3a0e2b18b2c7396c59369
Will Heitman <wheitman@rai-inst.com>
2025-08-11 11:28:58 -0400

    Speed up position readings using GroupSyncRead

--- factr_interface/dynamixel_test.py
+++ factr_interface/dynamixel_test.py
@@ -1,21 +1,27 @@
-from dynamixel_sdk import PacketHandler, PortHandler, COMM_SUCCESS
+from dynamixel_sdk import PacketHandler, PortHandler, COMM_SUCCESS, GroupSyncRead
 import numpy as np
+from time import time
 
 # FACTR and GELLO expect IDs from 1-8
 dynamixel_ids = list(range(1, 9))
 
 # See https://emanual.robotis.com/docs/en/dxl/x/xc330-t288/#control-table-of-ram-area
 ADDR_PRESENT_POSITION = 132
+LEN_PRESENT_POSITION = 4  # 4 bytes for position data
 
 # Initialize PortHandler instance
 # Set the port path
 # Get methods and members of PortHandlerLinux or PortHandlerWindows
-portHandler = PortHandler('/dev/ttyUSB0')
+portHandler = PortHandler("/dev/ttyUSB0")
 
 # Initialize PacketHandler instance
 # Set the protocol version
 # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
 packetHandler = PacketHandler(protocol_version=2.0)
+
+group_sync_read = GroupSyncRead(
+    portHandler, packetHandler, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION
+)
 
 # Open port
 if portHandler.openPort():
@@ -32,23 +38,31 @@ else:
     print("Failed to change the baudrate")
     quit()
 
+# Add all motors to the read group
+for dynamixel_id in dynamixel_ids:
+    if not group_sync_read.addParam(dynamixel_id):
+        print(f"Failed to add motor {dynamixel_id} to the sync read group")
+        exit()
+
 positions = np.zeros(8)
 
 try:
     while True:
+        start = time()
+
+        # Read the position of each motor in the group
+        dxl_comm_result = group_sync_read.txRxPacket()
+        if dxl_comm_result != COMM_SUCCESS:
+            print(f"{packetHandler.getTxRxResult(dxl_comm_result)}")
+
         for index, dynamixel_id in enumerate(dynamixel_ids):
 
-            # Get current position
-            dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, dynamixel_id, ADDR_PRESENT_POSITION)
-            if dxl_comm_result != COMM_SUCCESS:
-                print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-            elif dxl_error != 0:
-                print("%s" % packetHandler.getRxPacketError(dxl_error))
-            else:
-                # print(f"Motor {dynamixel_id} has position {dxl_present_position}")
-                positions[index] = dxl_present_position
+            positions[index] = group_sync_read.getData(
+                dynamixel_id, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION
+            )
 
         print(positions)
+        print(f"Took {time() - start} secs")
 
 except KeyboardInterrupt:
     print(f"User interrupted. Goodbye!")
